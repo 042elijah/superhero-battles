1d0c57f9cd1fb934fa5b23c5fd75e4d1f0ab0c6e
rserdah <94141015+rserdah@users.noreply.github.com>
2024-03-13 16:37:08 -0400

    Uploaded content for cards and basic damage functionality into this project (made in an isolated project and then moved here)

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,25 +1,15 @@
 import React from 'react';
-import logo from './logo.svg';
+import { Route, Routes } from 'react-router-dom';
 import './App.css';
+import GetAllImgs from './components/GetAllImgs';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <div>
+    <Routes>
+      <Route path='/heroes' element={<GetAllImgs />}></Route>
+    </Routes>
+  </div>
   );
 }
 
---
+++ frontend/src/components/GetAllImgs.tsx
@@ -0,0 +1,66 @@
+import React, { useEffect, useState } from 'react'
+import HeroCard from './HeroCard';
+import hero from '../model/hero';
+
+function GetAllImgs() {
+    const [heroes, setHeroes] = useState([] as hero[]);
+    const imageStart = 95;
+    const numImages = 40;
+    
+    useEffect(() => {
+        getHeroes();
+    }, []);
+
+    const getHeroes = async () => {
+        let newHeroes: hero[] = [];
+        for(let i = imageStart; i < imageStart + numImages; i++) {
+            const url = `https://www.superheroapi.com/api.php/122100753632232992/${i}`;
+            
+            await fetch(url)
+                .then(res => res.json())
+                .then((x: any) => {
+                    let h: hero = new hero({
+                        id: x.id,
+                        name: x.name,
+                        image: {
+                            url: x.image.url
+                        },
+                        powerstats: {
+                            intelligence: Number(x.powerstats.intelligence),
+                            strength: Number(x.powerstats.strength),
+                            speed: Number(x.powerstats.speed),
+                            durability: Number(x.powerstats.durability)
+                        },
+                        biography: {
+                            alignment: x.biography.alignment
+                        }
+                    });
+
+                    if(h && 
+                        h.powerstats.intelligence && 
+                        h.powerstats.strength && 
+                        h.powerstats.speed && 
+                        h.powerstats.durability) {
+                        newHeroes.push(h);
+                    }
+                })
+                .catch(console.error);
+        }
+
+        setHeroes(newHeroes);
+    };
+
+  return (
+      <div style={{
+        display: 'grid',
+        gridTemplateColumns: 'repeat(8, 1fr)',
+        gap: '0px'
+      }}>
+        {heroes && heroes.map((x: hero, idx: any) => {
+            return <HeroCard key={x.id} hero={x} />
+        })}
+    </div>
+  )
+}
+
+export default GetAllImgs
