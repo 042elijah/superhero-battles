7e0a1c7b8a57ed93a9376aba0f5797ae51217557
beam brain <elijah042@revature.net>
2024-03-14 22:00:26 -0700

    ~ commented out unused endpoints

--- backend/src/controller/battleRouter.js
+++ backend/src/controller/battleRouter.js
@@ -4,22 +4,22 @@ const router = express.Router();
 const pastBattleService = require("../service/pastBattleService");
 
 
-router.post("/", async (req, res) => {
-    res.status(200).json({ message: `post to battleground endpoint reached` })
-})
+// router.post("/", async (req, res) => {
+//     res.status(200).json({ message: `post to battleground endpoint reached` })
+// })
 
-router.get("/", async (req, res) => {
-    res.status(200).json({ message: `get battleground endpoint reached` })
-})
+// router.get("/", async (req, res) => {
+//     res.status(200).json({ message: `get battleground endpoint reached` })
+// })
 
 router.get("/leaderboard", async (req, res) => {
     res.status(200).json({ message: `get battleground leaderboard endpoint reached` })
 })
 
-router.get("/battles/:battleID", async (req, res) => {
-    let recievedQuery = req.params.battleID;
-    res.status(200).json({ message: `get query from battles with id: ${recievedQuery}` })
-})
+// router.get("/battles/:battleID", async (req, res) => {
+//     let recievedQuery = req.params.battleID;
+//     res.status(200).json({ message: `get query from battles with id: ${recievedQuery}` })
+// })
 
 // get the past battle records by username
 router.get("/record/:username", async(req, res) => {
