04d24c51f9bede6292d5709ded1e129e4007f4bc
beam brain <elijah042@revature.net>
2024-03-18 15:12:39 -0700

    ~ fix typo in filepath

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 import { Route, Routes, useParams } from 'react-router-dom';
 import './App.css';
 import GetAllImgs from './components/GetAllImgs';
-import RegisterContainer from './components/LogginAndRegister/LoginAndRegisterContainer';
+import RegisterContainer from './components/LoginAndRegister/LoginAndRegisterContainer';
 import Battle from './components/Battle/Battle';
 import ExploreUsersContainer from './components/ExploreUsersComp/ExploreUsersContainer';
 import UserProfile from './components/User/UserProfile';
