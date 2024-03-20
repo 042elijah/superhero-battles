686fba715c7f8154fe2da35a48554c9e69ab7124
rserdah <94141015+rserdah@users.noreply.github.com>
2024-03-19 21:36:05 -0400

    Fixed various small features

--- frontend/src/components/Home/Home.tsx
+++ frontend/src/components/Home/Home.tsx
@@ -1,12 +1,22 @@
 import React from 'react'
 import RegisterContainer from '../LoginAndRegister/LoginAndRegisterContainer'
+import { userActions } from '../Redux/slices/userSlice';
+import { useSelector } from 'react-redux';
 
 function Home() {
+  const username = useSelector((state: any) => state.token.username);
+
+
   return (
     <div>
       <RegisterContainer/>
       <h1>Welcome To SuperHero Battles</h1>
-      <h2>Please register or login</h2>
+      {
+        username ? 
+        <h2>{`Signed in as ${username}`}</h2>
+        :
+        <h2>Please register or login</h2>
+      }
     </div>
   )
 }
