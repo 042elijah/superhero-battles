8a7a911d10cd41906c50c265d9cea29883947db0
NathanMakesStuff <nathantigner518@gmail.com>
2024-03-21 08:49:23 -0700

    css changes for heroes page and user search

--- frontend/src/components/ExploreUsersComp/ExploreUsersDetailView.tsx
+++ frontend/src/components/ExploreUsersComp/ExploreUsersDetailView.tsx
@@ -1,17 +1,22 @@
 import React from 'react'
 import { Link } from 'react-router-dom';
+import './Users.css'
 
-function ExploreUsersDetailView(props:any) {
+function ExploreUsersDetailView(props: any) {
     return (
         <>
             {/* {props.users && <ul>{props.users}</ul>} */}
             <ul>
-            {
-                props.users && (props.users as any[]).map((user: any, idx: number) => {
-                    return <li key={'li_' + user.username}><Link to={`/users/${user.username}`}>{user.username}</Link></li>;
+                {
+                    props.users && (props.users as any[]).map((user: any, idx: number) => {
+                        return <li key={'li_' + user.username} style={{margin: '15px' }}>
+                            <Link to={`/users/${user.username}`} className='Users'>
+                                {user.username}
+                            </Link>
+                        </li>;
                 })
             }
-            </ul>
+        </ul >
         </>
     )
 }
